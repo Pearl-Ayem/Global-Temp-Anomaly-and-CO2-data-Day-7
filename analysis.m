2732989692f283d5695303e5c61fb62dfdd510f5
Pearl <pearlayem@gmail.com>
2019-01-24 12:37:52 -0800

    added ttest and 2 graphs

--- analysis.m
+++ analysis.m
@@ -22,10 +22,8 @@ title({'Global Temperature Anomaly' ,'from 1880-2017'})
 axis([1878,2018,-1,1.1])
 mgtanom = ~isnan(gtanom);
 maskboth = isnan(gtanom) | isnan(year);
-[coef,bint,r,rint,stats] = regress(gtanom,[ones(size(gtanom)) year]);
-R1 = stats(1)^2; 
-p1 = stats(3);
-[R,p] = corrcoef(gtanom(~maskboth),year(~maskboth));
+[coef,bint,~,~,~] = regress(gtanom,[ones(size(gtanom)) year]);
+[h,p,ci,stats] = ttest(year,gtanom);
 
 gtanomlinfit = coef(1)+coef(2).*year;
 gtanomlinfitbotlim = coef(1)+bint(2,1).*year;
@@ -35,8 +33,8 @@ plot(year(mgtanom),gtanomlinfit(mgtanom),'r');
 %plot(year(mgtanom),gtanomlinfitbotlim(mgtanom),'r--'); 
 %plot(year(mgtanom),gtanomlinfittoplim(mgtanom),'r--'); 
 text(1880,0.9,['Slope = ',num2str(round(coef(2),3)),...
-    '; p-value = ' num2str(p(2,1)) ','...
-    'Correlation Coefficient = ' num2str(R(2,1)) ])
+    '; p-value = ' num2str(p) ','...
+    'Tstat = ' num2str(stats.tstat)])
 hold off
 
 
@@ -48,10 +46,9 @@ title({'Atmospheric CO2 ' ,'from 1959-2017'})
 axis([1955,2018,300,420])
 mgtanom = ~isnan(CO2);
 maskboth = isnan(CO2) | isnan(year);
-[coef,bint,r,rint,stats] = regress(CO2,[ones(size(CO2)) year]);
-R1 = stats(1)^2; 
-p1 = stats(3);
-[R,p] = corrcoef(CO2(~maskboth),year(~maskboth));
+
+[coef,bint,~,~,~] = regress(CO2,[ones(size(CO2)) year]);
+[h,p,ci,stats] = ttest(year,CO2);
 
 gtanomlinfit = coef(1)+coef(2).*year;
 gtanomlinfitbotlim = coef(1)+bint(2,1).*year;
@@ -61,6 +58,6 @@ plot(year(mgtanom),gtanomlinfit(mgtanom),'r');
 %plot(year(mgtanom),gtanomlinfitbotlim(mgtanom),'r--'); 
 %plot(year(mgtanom),gtanomlinfittoplim(mgtanom),'r--'); 
 text(1955,410,['Slope = ',num2str(round(coef(2),3)),...
-    '; p-value = ' num2str(p(2,1)) ','...
-    'Correlation Coefficient = ' num2str(R(2,1)) ])
+    '; p-value = ' num2str(p) ','...
+    'Tstat = ' num2str(stats.tstat)])
 hold off
