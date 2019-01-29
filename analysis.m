0954b8990eee2258be687ccc292fc1214bf3bbcb
Pearl <pearlayem@gmail.com>
2019-01-29 10:20:49 -0800

    day 7 excel stats updated, raw graphs completed

--- analysis.m
+++ analysis.m
@@ -23,7 +23,8 @@ axis([1878,2018,-1,1.1])
 mgtanom = ~isnan(gtanom);
 maskboth = isnan(gtanom) | isnan(year);
 [coef,bint,~,~,~] = regress(gtanom,[ones(size(gtanom)) year]);
-[h,p,ci,stats] = ttest(year,gtanom);
+[h,p,ci,stats] = ttest(gtanom);
+
 
 gtanomlinfit = coef(1)+coef(2).*year;
 gtanomlinfitbotlim = coef(1)+bint(2,1).*year;
@@ -48,13 +49,52 @@ mgtanom = ~isnan(CO2);
 maskboth = isnan(CO2) | isnan(year);
 
 [coef,bint,~,~,~] = regress(CO2,[ones(size(CO2)) year]);
-[h,p,ci,stats] = ttest(year,CO2);
 
 gtanomlinfit = coef(1)+coef(2).*year;
 gtanomlinfitbotlim = coef(1)+bint(2,1).*year;
 gtanomlinfittoplim = coef(1)+bint(2,2).*year;
+[h,p,ci,stats] = ttest(CO2);
+
+
 hold on
 plot(year(mgtanom),gtanomlinfit(mgtanom),'r'); 
+%plot(year(mgtanom),gtanomlinfitbotlim(mgtanom),'r--'); 
+%plot(year(mgtanom),gtanomlinfittoplim(mgtanom),'r--'); 
+text(1955,410,['Slope = ',num2str(round(coef(2),3)),...
+    '; p-value = ' num2str(p) ','...
+    'Tstat = ' num2str(stats.tstat)])
+hold off
+
+%% CORRELATION BETWEEN CO2 AND TEMP
+
+overlap = zeros();
+for i =1:length(year)
+    if ~(isnan(CO2(i)) || isnan(gtanom(i)))
+        overlap(i,1)= year(i);
+        overlap(i,2)=gtanom(i);
+        overlap(i,3)=CO2(i);
+    end
+end
+
+figure
+plot(overlap(:,3),overlap(:,2),'k'); 
+xlabel('Year'); 
+ylabel('CO_2 (ppm)')
+title({'Global Temperature Anomaly VS' ,'Atmospheric CO2 from 1959-2017'})
+axis([1955,2018,300,420])
+mgtanom = ~isnan(overlap(:,2));
+maskboth = isnan(overlap(:,2)) | isnan(overlap(:,3));
+
+[coef,bint,~,~,~] = regress(overlap(:,2),[ones(size(overlap(:,2))) overlap(:,3)]);
+
+gtanomlinfit = coef(1)+coef(2).*overlap(:,3);
+gtanomlinfitbotlim = coef(1)+bint(2,1).*overlap(:,3);
+gtanomlinfittoplim = coef(1)+bint(2,2).*overlap(:,3);
+[h,p,ci,stats] = ttest(overlap(:,2));
+
+
+hold on
+plot(overlap(:,3),gtanomlinfit,'r'); 
 %plot(year(mgtanom),gtanomlinfitbotlim(mgtanom),'r--'); 
 %plot(year(mgtanom),gtanomlinfittoplim(mgtanom),'r--'); 
 text(1955,410,['Slope = ',num2str(round(coef(2),3)),...
